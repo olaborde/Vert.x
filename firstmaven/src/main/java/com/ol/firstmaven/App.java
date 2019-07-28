690a08dc69ac366caef9ce1bc1e489ab9c152640
Osse Laborde <oml007@live.com>
2019-07-27 23:01:34 -0400

    Setup maven app for vertx

--- firstmaven/src/main/java/com/ol/firstmaven/App.java
+++ firstmaven/src/main/java/com/ol/firstmaven/App.java
@@ -1,13 +1,32 @@
 package com.ol.firstmaven;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends AbstractVerticle
 {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new App());
+    }
+    
+    @Override
+    public void start() {
+    	LOGGER.info("Verticle App started");
+    }
+    
+    @Override
+    public void stop() {
+    	
+    	LOGGER.info("Verticle App stopped");
+    	
     }
 }
